85c8df42b00d12a2a4adfee22f02cf275ffe606f
Ulyana_Ovcharenko <malish26rus@mail.ru>
2022-11-27 19:37:36 +0300

    001

--- work_001/triangula_number.java
+++ work_001/triangula_number.java
@@ -1,10 +1,23 @@
 public class triangula_number {
+   
+        public static void main(String [] args) {
+        int c = 5;
+        for (int i = 0, num = 1; i < c; i++) {
+            for (int l = c - i; l > 0; l--) {
+                System.out.print("  ");
+            }
+            for (int l = 0; l <= i; l++) {
+                if (num < 11) {
+                    System.out.print(num + "   ");
+                    num += 2;
+                } else {
+                    System.out.print(num + "  ");
+                    num += 2;
+                }
+            }
+            System.out.println();
+        }
+        }
     
-    public int triangleF(int n) {
-
-        if (n == 1)
-            return 1;
-        else
-            return (n + triangleF(n - 1));
-    }
 }
+
